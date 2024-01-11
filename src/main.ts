03f223631a08b0ededad8c6470da2fcfa7cc926b
Ross Gardiner <github@public.rossng.eu>
2024-01-11 20:39:06 +0100

    Render a triangle

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,78 @@
-console.log("hello world");
+import redFragWGSL from "bundle-text:./frag.wgsl";
+import triangleVertWGSL from "bundle-text:./vert.wgsl";
+
+async function init() {
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    console.error("Could not get GPU adapter");
+    return;
+  }
+  const device = await adapter.requestDevice();
+
+  const canvas = document.getElementById("canvas");
+
+  if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
+    console.error("Could not get canvas");
+    return;
+  }
+
+  const context = canvas.getContext("webgpu") as GPUCanvasContext;
+  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
+    device,
+    format: presentationFormat,
+    alphaMode: "opaque",
+  });
+
+  const pipeline = device.createRenderPipeline({
+    layout: "auto",
+    vertex: {
+      module: device.createShaderModule({
+        code: triangleVertWGSL,
+      }),
+      entryPoint: "main",
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: redFragWGSL,
+      }),
+      entryPoint: "main",
+      targets: [
+        {
+          format: presentationFormat,
+        },
+      ],
+    },
+    primitive: {
+      topology: "triangle-list",
+    },
+  });
+
+  function frame() {
+    const commandEncoder = device.createCommandEncoder();
+    const textureView = context.getCurrentTexture().createView();
+
+    const renderPassDescriptor: GPURenderPassDescriptor = {
+      colorAttachments: [
+        {
+          view: textureView,
+          clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+          loadOp: "clear",
+          storeOp: "store",
+        },
+      ],
+    };
+
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setPipeline(pipeline);
+    passEncoder.draw(3, 1, 0, 0);
+    passEncoder.end();
+
+    device.queue.submit([commandEncoder.finish()]);
+    requestAnimationFrame(frame);
+  }
+
+  requestAnimationFrame(frame);
+}
+
+init();
