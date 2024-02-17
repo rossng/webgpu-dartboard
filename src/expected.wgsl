0118ebb219542d0e4fb19f74b6b8df200dc62b8b
Ross Gardiner <github@public.rossng.eu>
2024-02-18 00:55:37 +0100

    Render grid of expected scores by aim target

---
+++ src/expected.wgsl
@@ -0,0 +1,61 @@
+@group(0) @binding(0) var<storage, read_write> data: array<f32>;
+@group(0) @binding(1) var<uniform> dims: vec2u;
+@group(0) @binding(2) var<storage, read> dartboard: array<u32>;
+
+const WORKGROUP_SIZE: u32 = 64;
+
+@compute @workgroup_size(WORKGROUP_SIZE) fn computeSomething(
+  @builtin(workgroup_id) workgroup_id: vec3<u32>,
+  @builtin(global_invocation_id) global_id: vec3<u32>,
+  @builtin(num_workgroups) num_workgroups: vec3<u32>,
+  @builtin(local_invocation_index) local_invocation_index: u32,
+) {
+  let length = arrayLength(&data);
+  let total_num_workgroups = num_workgroups.x * num_workgroups.y * num_workgroups.z;
+  let count = length / (WORKGROUP_SIZE * total_num_workgroups);
+  let ignore = dims.x;
+
+  let workgroup_index =  
+    workgroup_id.x +
+    workgroup_id.y * num_workgroups.x +
+    workgroup_id.z * num_workgroups.x * num_workgroups.y;
+
+  let global_invocation_index =
+     workgroup_index * WORKGROUP_SIZE +
+     local_invocation_index;
+
+  let start = global_invocation_index * count;
+  let end = min(start + count, length);
+
+  for (var i: u32 = start; i < end; i = i + 1) {
+    let edge_length = u32(sqrt(f32(length)));
+    let x = i % edge_length;
+    let y = i / edge_length;
+
+
+    var total_probability: f32 = 0.0;
+    var total_score: f32 = 0.0;
+
+    for (var j: u32 = 0; j < edge_length; j = j + 1) {
+      for (var k: u32 = 0; k < edge_length; k = k + 1) {
+        let gaussian = gaussian2D(f32(k), f32(j), f32(x), f32(y), 40, 40);
+        // let gaussian = gaussian2D(f32(x), f32(y), f32(j), f32(k), 100, 100);
+        let score = f32(dartboard[j * edge_length + k]);
+        total_probability = total_probability + gaussian;
+        total_score = total_score + gaussian * score;
+        // data[j * edge_length + k] = gaussian * score;
+      }
+    }
+
+    // data[i] = f32(total_score); // f32(i); // total_score / total_probability;
+    // data[i] = f32(i);
+    data[i] = select(total_score / total_probability, 0.0, total_probability == 0.0);
+  }
+}
+
+fn gaussian2D(x: f32, y: f32, mu_x: f32, mu_y: f32, sigma_x: f32, sigma_y: f32) -> f32 {
+  let coef: f32 = 1.0 / (2.0 * 3.14159265 * sigma_x * sigma_y);
+  let exp_part: f32 = exp(-((x - mu_x) * (x - mu_x) / (2.0 * sigma_x * sigma_x) + (y - mu_y) * (y - mu_y) / (2.0 * sigma_y * sigma_y)));
+  return coef * exp_part;
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import shader from "bundle-text:./shader.wgsl";
+import expected from "bundle-text:./expected.wgsl";
+import weightedGrid from "bundle-text:./weighted-grid.wgsl";
 import { makeDartboard } from "./dartboard";
 
 const width = 500;
@@ -12,12 +13,12 @@ async function init() {
   }
 
   const module = device.createShaderModule({
-    label: "doubling compute module",
-    code: shader,
+    label: "weighted grid module",
+    code: weightedGrid,
   });
 
   const pipeline = device.createComputePipeline({
-    label: "doubling compute pipeline",
+    label: "weighted grid pipeline",
     layout: "auto",
     compute: {
       module,
@@ -95,7 +96,7 @@ async function init() {
   const result = new Float32Array(resultBuffer.getMappedRange().slice(0));
   resultBuffer.unmap();
 
-  const canvas = document.getElementById("canvas");
+  const canvas = document.getElementById("weighted-grid");
   if (!(canvas instanceof HTMLCanvasElement)) {
     return;
   }
@@ -152,8 +153,136 @@ async function initDartboard() {
   ctx.putImageData(imageData, 0, 0);
 }
 
+async function computeExpected() {
+  const adapter = await navigator.gpu?.requestAdapter();
+  const device = await adapter?.requestDevice();
+  if (!device) {
+    fail("need a browser that supports WebGPU");
+    return;
+  }
+
+  const module = device.createShaderModule({
+    label: "expected score module",
+    code: expected,
+  });
+
+  const pipeline = device.createComputePipeline({
+    label: "expected score pipeline",
+    layout: "auto",
+    compute: {
+      module,
+      entryPoint: "computeSomething",
+    },
+  });
+
+  const input = new Float32Array(width * width);
+
+  // create a buffer on the GPU to hold our computation
+  // input and output
+  const workBuffer = device.createBuffer({
+    label: "work buffer",
+    size: input.byteLength,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST,
+  });
+  // Copy our input data to that buffer
+  device.queue.writeBuffer(workBuffer, 0, input);
+
+  // create a buffer on the GPU to get a copy of the results
+  const resultBuffer = device.createBuffer({
+    label: "result buffer",
+    size: input.byteLength,
+    usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+  });
+
+  const uniformData = new Uint32Array([width, width]); // Replace x and y with your values
+  const uniformBuffer = device.createBuffer({
+    size: uniformData.byteLength,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+  });
+  device.queue.writeBuffer(uniformBuffer, 0, uniformData);
+
+  const dartboardScore = makeDartboard(width);
+  const dartboardBuffer = device.createBuffer({
+    label: "dartboard buffer",
+    size: dartboardScore.byteLength,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  });
+  device.queue.writeBuffer(dartboardBuffer, 0, dartboardScore);
+
+  // Setup a bindGroup to tell the shader which
+  // buffer to use for the computation
+  const bindGroup = device.createBindGroup({
+    label: "bindGroup for work buffer",
+    layout: pipeline.getBindGroupLayout(0),
+    entries: [
+      { binding: 0, resource: { buffer: workBuffer } },
+      { binding: 1, resource: { buffer: uniformBuffer } },
+      { binding: 2, resource: { buffer: dartboardBuffer } },
+    ],
+  });
+
+  // Encode commands to do the computation
+  const encoder = device.createCommandEncoder({
+    label: "doubling encoder",
+  });
+  const pass = encoder.beginComputePass({
+    label: "doubling compute pass",
+  });
+  pass.setPipeline(pipeline);
+  pass.setBindGroup(0, bindGroup);
+  pass.dispatchWorkgroups(100);
+  pass.end();
+
+  // Encode a command to copy the results to a mappable buffer.
+  encoder.copyBufferToBuffer(workBuffer, 0, resultBuffer, 0, resultBuffer.size);
+
+  // Finish encoding and submit the commands
+  const commandBuffer = encoder.finish();
+  device.queue.submit([commandBuffer]);
+
+  // Read the results
+  const start = Date.now();
+  console.log("start compute expected score");
+  await resultBuffer.mapAsync(GPUMapMode.READ);
+  const result = new Float32Array(resultBuffer.getMappedRange().slice(0));
+  console.log("finish compute expected score", (Date.now() - start) / 1000, result);
+  resultBuffer.unmap();
+
+  const canvas = document.getElementById("expected-score");
+  if (!(canvas instanceof HTMLCanvasElement)) {
+    return;
+  }
+  canvas.height = width;
+  canvas.width = width;
+  const ctx = canvas.getContext("2d");
+  if (!ctx) {
+    return;
+  }
+
+  const imageData = ctx.createImageData(width, width);
+
+  const max = result.reduce((a, b) => Math.max(a, b), 0);
+
+  // Map float values to pixel data
+  for (let i = 0; i < result.length; i++) {
+    // Example mapping: float value to grayscale intensity
+    // Modify this mapping based on your data's range and desired visual representation
+    const intensity = Math.floor(result[i] * (1 / max) * 255);
+
+    // Each pixel requires 4 slots in the array (R, G, B, A)
+    imageData.data[i * 4 + 0] = intensity; // R
+    imageData.data[i * 4 + 1] = intensity; // G
+    imageData.data[i * 4 + 2] = intensity; // B
+    imageData.data[i * 4 + 3] = 255; // A, fully opaque
+  }
+
+  // Draw the imageData onto the canvas
+  ctx.putImageData(imageData, 0, 0);
+}
+
 init();
 initDartboard();
+computeExpected();
 
 function fail(msg: string) {
   // eslint-disable-next-line no-alert
